a03e42f8ef4ae7a663234996771ed0c897a86d3e
joaopluigi <joao.luigi@gmail.com>
2018-09-05 10:29:34 -0300

    add fn logic

--- import-pi-logs/handler.py
+++ import-pi-logs/handler.py
@@ -1,9 +1,38 @@
+import os
+import sys
 import json
 
 
 def handle(req):
-    try: 
-        json_req = json.loads(req)
-        print json.dumps(json_req)
+    try:
+
+        # Open log file
+        f = open('pi.log', 'r+')
+
+        # Return var
+        ret = {}
+
+        if os.getenv('Http_Method') == 'POST':
+
+            # Parse request data
+            json_req = json.loads(req)
+
+            # Write log to file
+            f.write(str(json_req['log']))
+
+            # Set return message
+            ret = {'message': 'Log updated!'}
+
+        else:
+
+            # Get log file string
+            ret = {'log': f.read()}
+            
+
+        # Return object
+        print json.dumps(ret)
+
     except:
-        pass
+
+        # Return error
+        print "Unexpected error:", sys.exc_info()[0]
