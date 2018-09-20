84df088a798b21ba605c02d4f27817dcaeef8758
joaopluigi <joao.luigi@gmail.com>
2018-09-20 15:47:44 -0300

    add amp website for testing purpose
    Signed-off-by: joaopluigi <joao.luigi@gmail.com>

--- pi-logs/handler.py
+++ pi-logs/handler.py
@@ -13,36 +13,33 @@ def handle(req):
         if os.getenv('Http_Method') == 'POST':
 
             # Open log file for write
-            f = open('/tmp/pi.log', 'w')
+            with open('/tmp/pi.log', 'w') as f:
 
-            # Parse request data
-            json_req = json.loads(req)
+                # Parse request data
+                json_req = json.loads(req)
 
-            # Write log to file
-            f.write(str(json_req['log']))
+                # Write log to file
+                f.write(str(json_req['log']))
 
-            # Set return message
-            ret = {'message': 'ok'}
+                # Set return message
+                ret = {'message': 'ok'}
 
-            # parse to JSON
-            ret = json.dumps(ret)
+                # parse to JSON
+                ret = json.dumps(ret)
 
         else:
 
             # Open log file for read
-            f = open('/tmp/pi.log', 'r')
+            with open('/tmp/pi.log', 'r') as f:
 
-            # Replace \n with <br> tag
-            logs = (f.read()).replace('\n', '<br>')
-
-            # Add HTML template
-            logs = '<html><head></head><body>{0}</body></html>'.format(logs)
-
-            # Set to return object
-            ret = logs
-
-        # Close file
-        f.close()
+                # Replace \n with <br> tag
+                logs = (f.read()).replace('\n', '<br>')
+    
+                # Add HTML template
+                logs = '<html><head></head><body>{0}</body></html>'.format(logs)
+    
+                # Set to return object
+                ret = logs
 
         # Return object
         print ret
